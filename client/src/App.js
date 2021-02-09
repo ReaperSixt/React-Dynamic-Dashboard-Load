9b5f1b759f302f995cf7221211b195c6292939ba
Konstantin Koynov <kkoynov@infragistics.com>
2021-02-09 18:58:56 +0200

    Moving setting the server url out of public.html to App.js.

--- client/src/App.js
+++ client/src/App.js
@@ -4,11 +4,15 @@ import { Layout } from "./components/Layout";
 import { Home } from "./components/Home";
 import { Linking } from "./components/Linking";
 import { Filters } from "./components/Filters";
-
 import "./custom.css";
+const $ = window.$;
 
 export default class App extends Component {
   static displayName = App.name;
+  constructor() {
+    super();
+    $.ig.RevealSdkSettings.setBaseUrl("http://localhost:8080/");
+  }
 
   render() {
     return (
